a6a40c114f6e38b4dd5746efadc28d6890ddf7dc
Luis Arturo Mendoza <arturo10.mr@gmail.com>
2018-04-18 21:21:53 -0500

    Update Readme and change name database in Mongo

--- server/config/development.js
+++ server/config/development.js
@@ -4,7 +4,7 @@ module.exports = {
   seed: false,
   db: {
     //crear BD
-    url: 'mongodb://localhost/omgtodo'
+    url: 'mongodb://localhost/omgtask'
     //url: 'mongodb://localhost/funmath-dev'
   }
 };
